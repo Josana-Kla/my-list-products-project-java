756b2253172e435eb275b96379bf362deae4f388
JosanaKla <josana.0205@gmail.com>
2021-12-12 09:49:58 -0300

    h2 database

--- src/main/java/com/devnewproject/mylistproductsproject/entities/Category.java
+++ src/main/java/com/devnewproject/mylistproductsproject/entities/Category.java
@@ -2,21 +2,25 @@ package com.devnewproject.mylistproductsproject.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
+@Entity
 public class Category implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
     @JsonIgnore
+    @OneToMany(mappedBy = "category")
     private List<Product> products = new ArrayList<>();
 
     public Category() {
